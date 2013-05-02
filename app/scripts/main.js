20610951b240024a050f02c07f931ba3f10efbc0
Oskar Rough <oskarrough@gmail.com>
2013-05-02 21:28:37 +0200

    Clean up + gallery controls

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -29,4 +29,46 @@ $(document).ready(function() {
 			gutter: 30
 		});
 	}
+
+	//
+	// 
+	function scrollToTop(speed) {
+		$('.js-scrollToTop').text('Rewinding');
+		
+		$('html, body').animate({
+			scrollTop: 0
+		}, 'slow', function(){
+			$('.js-scrollToTop').text('Rewind');
+		});
+		
+		return false;
+	}
+	function scrollToBottom(speed) {
+		$('.js-scrollToBottom').text('Playing');
+		
+		$('html, body').animate({
+			scrollTop: $(document).height()
+		}, 40000, 'linear');
+		
+		return false;
+	}
+	function scrollStop() {
+		$('html, body').stop();
+		$('.js-scrollToBottom').text('Play');
+		
+		return false;
+	}
+
+
+
+	$('.js-scrollToTop').click(function(event){
+		scrollToTop();
+	});
+	$('.js-scrollToBottom').click(function(event){
+		scrollToBottom();
+	});
+	$('.js-stop').click(function(event){
+		scrollStop();
+	});
+
 });
