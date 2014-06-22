43f50a4150383da325471b08d0aab6b0fd3476f5
Oskar Rough <oskarrough@gmail.com>
2014-06-22 15:28:39 +0100

    Redo masonry

--- app/scripts/program.js
+++ app/scripts/program.js
@@ -2,29 +2,39 @@
  * Dynamic grid with Masonry
  */
 
-function goGrid() {
-	var $container = $('#Masonry');
-	if ($container.length > 0) {
-		$container.prepend('<div class="grid-sizer"></div><div class="gutter-sizer"></div>');
-		$container.masonry({
-			columnWidth: '.grid-sizer',
-			gutter: '.gutter-sizer',
-			itemSelector: '.Artist'
-		});
-	}
-}
-
 $(function () {
 
+	// check for masonry and start
 	var $container = $('#Masonry');
-	if ($container.length > 0) {
-		goGrid();
+	if (!$container.length) {
+		return false;
 	}
 
+	runMasonry();
+
+});
+
+function runMasonry() {
+
+	// do masonry
+	var $container = $('#Masonry');
+	$container.prepend('<div class="grid-sizer"></div><div class="gutter-sizer"></div>');
+	$container.masonry({
+		columnWidth: '.grid-sizer',
+		gutter: '.gutter-sizer',
+		itemSelector: '.Artist'
+	});
+
+	// layout again after all images are loaded
+	$container.imagesLoaded( function(){
+		$container.masonry();
+	});
+
+	// fade in each masonry item as they load
 	var $artists = $('.Masonry').find('.Artist');
 	$artists.each(function(index, el){
 		$(this).imagesLoaded( function() {
 			$(el).addClass('is-loaded');
 		});
 	});
-});
+}
