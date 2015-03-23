480f32f927070884319fa8a7ac8471795e1b9c24
Oskar Rough <oskarrough@gmail.com>
2015-03-23 11:11:04 +0100

    Update splash design

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -31,7 +31,7 @@ $(document).ready(function() {
 	// });
 
 	if ($('.Splash').length) {
-		console.log('splashy');
+
 		// initializes BigVideo
 		var BV = new $.BigVideo({
 			controls: false,
@@ -41,7 +41,7 @@ $(document).ready(function() {
 
 		// if (!Modernizr.touch) {}
 		BV.show([
-			// { type: 'video/mp4',  src: 'images/2015/alive-bg.mp4' },
+			{ type: 'video/mp4',  src: 'images/2015/alive-bg.mp4' },
 			{ type: 'video/webm', src: 'images/2015/alive-bg.webm' },
 			{ type: 'video/ogg',  src: 'images/2015/alive-bg.ogv' }
 		]);
