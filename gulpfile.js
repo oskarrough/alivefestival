c13f824f4cab0cab46bf9c222650be4e31531235
Oskar Rough <oskarrough@gmail.com>
2015-05-14 11:10:36 +0200

    Don't minify images again

--- gulpfile.js
+++ gulpfile.js
@@ -62,14 +62,14 @@ gulp.task('html', ['views', 'styles'], function() {
 
 gulp.task('images', function() {
 	return gulp.src('app/images/**/*')
-		.pipe($.cache($.imagemin({
-			progressive: true,
-			interlaced: true,
+		// .pipe($.cache($.imagemin({
+		// 	progressive: true,
+		// 	interlaced: true,
 
-			// don't remove IDs from SVGs, they are often used
-			// as hooks for embedding and styling
-			svgoPlugins: [{ cleanupIDs: false }]
-		})))
+		// 	// don't remove IDs from SVGs, they are often used
+		// 	// as hooks for embedding and styling
+		// 	svgoPlugins: [{ cleanupIDs: false }]
+		// })))
 		.pipe(gulp.dest('dist/images'));
 });
 
