b7b5d08cc74113b330810d644a5b930a485d053f
Oskar Rough <oskarrough@gmail.com>
2016-02-05 16:41:56 +0100

    Another backup

--- app/news/index/route.js
+++ app/news/index/route.js
@@ -2,6 +2,9 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
 	model() {
-		return this.store.findAll('post');
+		return Ember.RSVP.hash({
+			page: this.store.findRecord('page', 120),
+			posts: this.store.findAll('post')
+		});
 	}
 });
