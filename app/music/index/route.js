18e7a0c08c80237388cb1c4cb540fee9506af2e1
Oskar Rough <oskarrough@gmail.com>
2016-02-23 00:39:53 +0100

    Improvements all around

--- app/music/index/route.js
+++ app/music/index/route.js
@@ -4,10 +4,10 @@ const {Route} = Ember;
 
 export default Route.extend({
 	model() {
-		 // /artists?filter[cat]=12 or /posts?filter[category_name]=jazz_funk
+		// /artists?filter[cat]=12 or /posts?filter[category_name]=jazz_funk
 		return Ember.RSVP.hash({
 			page: this.store.findRecord('page', 14),
-			artists: this.store.query('artist', {per_page: 999})
+			artists: this.store.query('artist', {per_page: 99})
 		});
 	}
 });
