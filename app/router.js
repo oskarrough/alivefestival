bd64f6d6abfbb8900f52a171a7b27d9299833104
Oskar Rough <oskarrough@gmail.com>
2016-02-23 00:38:56 +0100

    Add 404

--- app/router.js
+++ app/router.js
@@ -5,11 +5,11 @@ const Router = Ember.Router.extend({
   location: config.locationType
 });
 
-Router.map(function() {
-  this.route('news', {path: 'nyheder'}, function() {
+Router.map(function () {
+  this.route('news', {path: 'nyheder'}, function () {
     this.route('post', {path: ':post_slug'});
   });
-  this.route('music', {path: 'musik'}, function() {
+  this.route('music', {path: 'musik'}, function () {
     this.route('artist', {path: ':artist_slug'});
   });
   this.route('adventure', {path: 'eventyr'});
@@ -18,9 +18,10 @@ Router.map(function() {
     this.route('contact', {path: 'kontakt'});
     this.route('history', {path: 'historie'});
   });
-  this.route('english');
   this.route('memory', {path: 'lykkehjulet'});
   this.route('tickets', {path: 'billetter'});
+  this.route('english');
+  this.route('404', {path: '/*wildcard'});
 });
 
 export default Router;
