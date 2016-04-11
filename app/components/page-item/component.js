7376926c89b62f17aa1fb4396825dd675a57cdd9
Kimberly Gessner <kimberlygessner@yahoo.de>
2016-04-12 00:16:35 +0200

    billetter discofun feature

--- app/components/page-item/component.js
+++ app/components/page-item/component.js
@@ -3,5 +3,11 @@ import Ember from 'ember';
 export default Ember.Component.extend({
 	layout: Ember.inject.service(),
 	classNames: ['Page'],
-	classNameBindings: ['layout.billetter']
+	classNameBindings: ['layout.billetter','partyOn'],
+
+	actions: {
+		partyOn() {
+			this.toggleProperty('partyOn');
+		}
+	}
 });
