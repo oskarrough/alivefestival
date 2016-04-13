cafb22238bfe539184f1807f473af21c3f4a4b8d
Oskar Rough <oskarrough@gmail.com>
2016-04-13 10:13:07 +0200

    Rename 'Eventyr' page to 'Sideprogram'

--- app/router.js
+++ app/router.js
@@ -13,7 +13,7 @@ Router.map(function () {
   this.route('music', {path: 'musik'}, function () {
     this.route('artist', {path: ':artist_slug'});
   });
-  this.route('adventure', {path: 'eventyr'});
+  this.route('adventure', {path: 'sideprogram'});
   this.route('guide');
   this.route('about', {path: 'om-alive'}, function () {
     this.route('contact', {path: 'kontakt'});
