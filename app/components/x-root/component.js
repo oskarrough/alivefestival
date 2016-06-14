f3e4e6705533d7bc58544c88b964538de2b7ae68
Oskar Rough <oskarrough@gmail.com>
2016-06-14 17:13:10 +0200

    Add loading-screen

--- app/components/x-root/component.js
+++ app/components/x-root/component.js
@@ -1,6 +1,7 @@
+/* global document */
 import Ember from 'ember';
 
-const {Component, inject} = Ember;
+const {Component, inject, run} = Ember;
 
 export default Component.extend({
 	layout: inject.service(),
@@ -9,5 +10,18 @@ export default Component.extend({
 		toggleAside() {
 			this.toggleProperty('layout.showAside');
 		}
+	},
+
+	didInsertElement() {
+		run.scheduleOnce('afterRender', () => this.removeLoadingScreen());
+	},
+
+	// Remove our dummy app with inline styles.
+	removeLoadingScreen() {
+		const dummy = document.querySelector('.LoadingScreen');
+		if (!dummy) {
+			return;
+		}
+		dummy.parentNode.removeChild(dummy);
 	}
 });
