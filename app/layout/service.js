96c735da68dcd89a15539bd6f37dcbfb005b283c
Oskar Rough <oskar@rough.dk>
2017-03-21 20:11:34 +0100

    Style layout to sidebar without bg

--- app/layout/service.js
+++ app/layout/service.js
@@ -1,15 +1,12 @@
 /* global document */
 import Ember from 'ember';
-// import Cookies from 'ember-cli-js-cookie';
 
 const BREAKPOINT = 740;
 const {computed, on} = Ember;
 
 export default Ember.Service.extend({
 	showAside: false,
-
 	billetter: false,
-
 	simplify: false,
 
 	setInitialWidth: on('init', function () {
@@ -31,24 +28,3 @@ export default Ember.Service.extend({
 		}
 	}
 });
-
-// // isPanelLeftVisible: true,
-// isPanelLeftVisible: computed({
-// 	get() {
-// 		if (this.get('smallScreen')) {
-// 			return false;
-// 		}
-
-// 		// if the cookie is undefined (not set), it returns true
-// 		// or if the cookie has 'true' as value, return true
-// 		const noCookie = Cookies.get('isPanelOpen') === undefined;
-// 		return noCookie || Cookies.get('isPanelOpen') === 'true';
-// 	},
-// 	set(key, value) {
-// 		Cookies.set('isPanelOpen', value, {expires: 7});
-// 		return value;
-// 	}
-// }),
-// togglePanelLeft() {
-// 	this.toggleProperty('isPanelLeftVisible');
-// }
