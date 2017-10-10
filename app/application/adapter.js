b53d0d543d3e23554eb6e87f758603d72387b510
Oskar Rough <oskar@rough.dk>
2017-10-10 23:17:55 +0200

    Change API endpoint to https

--- app/application/adapter.js
+++ app/application/adapter.js
@@ -1,6 +1,6 @@
 import DS from 'ember-data';
 
 export default DS.RESTAdapter.extend({
-	host: 'http://2016.alivefestival.dk',
+	host: 'https://cms.alivefestival.dk',
 	namespace: 'wp-json/wp/v2'
 });
