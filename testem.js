bc5c65df36f011ccf3ceb8018182eb194d3d9567
Oskar Rough <oskar@rough.dk>
2017-10-11 11:08:46 +0200

    Upgrade all the things

--- testem.js
+++ testem.js
@@ -3,11 +3,18 @@ module.exports = {
   "framework": "qunit",
   "test_page": "tests/index.html?hidepassed",
   "disable_watching": true,
-  "launch_in_ci": [
-    "PhantomJS"
+  launch_in_dev: [
+    'Chrome'
   ],
-  "launch_in_dev": [
-    "PhantomJS",
-    "Chrome"
-  ]
+  browser_args: {
+    Chrome: {
+      mode: 'ci',
+      args: [
+        '--disable-gpu',
+        '--headless',
+        '--remote-debugging-port=9222',
+        '--window-size=1440,900'
+      ]
+    },
+  }
 };
