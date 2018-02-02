db9c690c9dd46958ae93b037aeec9f72a5d622a7
Oskar Rough <oskar@rough.dk>
2018-02-02 20:35:07 +0100

    Lint

--- app/program/controller.js
+++ app/program/controller.js
@@ -2,6 +2,9 @@ import {sort} from '@ember/object/computed'
 import Controller from '@ember/controller'
 
 export default Controller.extend({
-	sortDefinition: ['acf.festival_date'],
+	init() {
+		this._super(...arguments)
+		this.set('sortDefinition', ['acf.festival_date'])
+	},
 	sortedArtists: sort('model.artists', 'sortDefinition')
 })
