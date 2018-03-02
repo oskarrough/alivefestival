ea9d2ca6c8763c8e430338ec9d0c7f557636453e
Oskar Rough <oskar@rough.dk>
2018-03-02 16:04:02 +0100

    Manual caching oh wow

--- app/news/index/route.js
+++ app/news/index/route.js
@@ -3,9 +3,13 @@ import Route from '@ember/routing/route'
 
 export default Route.extend({
 	model() {
+		if (this.get('cached')) return this.get('cached')
 		return hash({
-			page: this.store.findRecord('page', 120),
-			posts: this.store.query('post', {per_page: 99})
+			posts: this.store.query('post', {per_page: 99}),
+			page: this.store.findRecord('page', 120)
+		}).then(model => {
+			this.set('cached', model)
+			return model
 		})
 	}
 })
