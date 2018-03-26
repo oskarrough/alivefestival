05ca3a4660cc47be8ed96ea9c224348fcaeee2e2
Oskar Rough <oskar@rough.dk>
2018-03-26 19:01:48 +0200

    Change to fade animation

--- app/transitions.js
+++ app/transitions.js
@@ -1,16 +1,16 @@
 export default function() {
 	// Set velocity.js defaults
 	this.setDefault({
-		duration: 500,
-		easing: [0.86, 0.0, 0.07, 1.0]
+		duration: 150,
+		// easing: [0.86, 0.0, 0.07, 1.0]
 	})
 
 	// Between Music and News index/detail
 	this.transition(
 		this.fromRoute(['news.index', 'music.index']),
 		this.toRoute(['news.post', 'music.artist']),
-		this.use('toLeft'),
-		this.reverse('toRight')
+		this.use('fade'),
+		this.reverse('fade')
 	)
 
 	// Between pages
