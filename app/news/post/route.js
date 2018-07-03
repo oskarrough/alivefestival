2059a4587cc3637b17e2ae6cc5928e02d618b156
Oskar Rough <oskar@rough.dk>
2018-07-03 13:42:18 +0200

    Add and disable time-schedule component

--- app/news/post/route.js
+++ app/news/post/route.js
@@ -3,7 +3,7 @@ import Route from '@ember/routing/route'
 export default Route.extend({
 	model(params) {
 		return this.store
-			.query('post', {filter: {name: params.slug}})
+			.query('post', {filter: {slug: params.slug}})
 			.then(query => query.get('firstObject'))
 	},
 	serialize(model) {
