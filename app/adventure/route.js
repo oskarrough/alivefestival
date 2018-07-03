5968cd46b3c526d537137247c2f62cf81b452271
Oskar Rough <oskar@rough.dk>
2018-07-03 15:58:46 +0200

    Redirect "dagsprogram" to "program"

--- app/adventure/route.js
+++ app/adventure/route.js
@@ -1,6 +1,9 @@
 import Route from '@ember/routing/route'
 
 export default Route.extend({
+	beforeModel() {
+		return this.replaceWith('program')
+	},
 	model() {
 		return this.store.findRecord('page', 43)
 	}
