476254c9a62b5267091518005ec1eb3045096996
Oskar Rough <oskar@rough.dk>
2018-10-17 13:20:49 +0200

    Make it possible to hide sections on a page

--- app/components/page-item/component.js
+++ app/components/page-item/component.js
@@ -1,10 +1,15 @@
-import {inject as service} from '@ember/service'
 import Component from '@ember/component'
+import {inject as service} from '@ember/service'
+import {computed, get} from '@ember/object'
 
 export default Component.extend({
 	layout: service(),
 	classNames: ['Page'],
 	classNameBindings: ['layout.billetter', 'partyOn'],
+
+	showSections: computed('page.acf.{sections,hide_sections}', function() {
+		return get(this, 'page.acf.sections') && !get(this, 'page.acf.hide_sections')
+	}),
 
 	// page: alivefestival/models/page
 
