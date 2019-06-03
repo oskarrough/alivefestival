7cef7faa3d9e9a5e6a554e97c27942836f0069c7
Oskar Rough <oskar@rough.dk>
2019-06-03 10:53:08 +0200

    Fix newsletter tests

--- tests/integration/components/newsletter-form/component-test.js
+++ tests/integration/components/newsletter-form/component-test.js
@@ -7,20 +7,8 @@ module('Integration | Component | newsletter-form', function(hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function(assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
-
     await render(hbs`{{newsletter-form}}`);
-
-    assert.equal(this.element.textContent.trim(), '');
-
-    // Template block usage:
-    await render(hbs`
-      {{#newsletter-form}}
-        template block text
-      {{/newsletter-form}}
-    `);
-
-    assert.equal(this.element.textContent.trim(), 'template block text');
+    // assert.equal(this.element.textContent.trim(), '');
+    assert.equal(this.element.querySelectorAll('input[type="submit"]').length, 1);
   });
 });
