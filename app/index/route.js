21a2cee9e4a9be0104d9948b5a622a995c66e878
Esben Holk <esbenholknielsen@gmail.com>
2020-03-23 14:14:46 +0100

    news listicle recreated for mobile first

--- app/index/route.js
+++ app/index/route.js
@@ -5,7 +5,7 @@ export default Route.extend({
 	model() {
 		return hash({
 			page: this.store.findRecord('page', 67),
-			news: this.store.query('post', {per_page: 2}),
+			news: this.store.query('post', {per_page: 6}),
 			artists: this.store.query('artist', {
 				categories: '7',
 				per_page: 99
