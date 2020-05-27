01eddcb14800db00914c624813461f31a6bdd4c8
Oskar Rough <oskar@rough.dk>
2020-05-27 12:01:59 +0200

    Add "FAQ" model

--- app/practical/route.js
+++ app/practical/route.js
@@ -4,11 +4,12 @@ import Route from '@ember/routing/route'
 export default Route.extend({
 	model() {
 		return hash({
-		page: this.store.findRecord('page', 2651),
-		practical: this.store.query('post', {
-			categories: 30
+			page: this.store.findRecord('page', 2651),
+			// practical: this.store.query('post', {
+			// 	categories: 30
+			// })
+			practical: this.store.findAll('faq')
 		})
-	})
 	}
 
 })
