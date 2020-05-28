aa42d8c4ae0903c510c60fbd8d7f98f3c92a3d96
Esben Holk <esbenholknielsen@gmail.com>
2020-05-28 19:36:50 +0200

    wrote to call FAQ instead of Post in Practical

--- app/practical/post/route.js
+++ app/practical/post/route.js
@@ -3,7 +3,7 @@ import Route from '@ember/routing/route'
 export default Route.extend({
 	model(params) {
 		return this.store
-			.query('post', {filter: {slug: params.slug}})
+			.query('faq', {filter: {slug: params.slug}})
 			.then(query => query.get('firstObject'))
 	},
 	serialize(model) {
